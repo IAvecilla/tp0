e5a297fc30d148093c0c307dddd9e52f347c941f
IAvecilla <iavecilla@fi.uba.ar>
2025-08-30 14:12:14 -0300

    No generar un dockerfile nuevo para verificar

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,11 +1,22 @@
-TEST_MESSAGE="${TEST_MESSAGE:-test}"
-docker build -t server-verifier -f server-verifier-dockerfile .
+#!/bin/bash
 
-echo "Testing server wih message: $TEST_MESSAGE"
-result=$(docker run --network tp0_testing_net --rm server-verifier sh -c "echo '$TEST_MESSAGE' | nc server 12345")
+# TEST_MESSAGE="${TEST_MESSAGE:-test}"
+# docker build -t server-verifier -f server-verifier-dockerfile .
+
+# echo "Testing server wih message: $TEST_MESSAGE"
+# result=$(docker run --network tp0_testing_net --rm server-verifier sh -c "echo '$TEST_MESSAGE' | nc server 12345")
+
+# if [ "$result" == "$TEST_MESSAGE" ]; then
+#     echo "action: test_echo_server | result: success"
+# else
+#     echo "action: test_echo_server | result: fail"
+# fi
+
+TEST_MESSAGE="${TEST_MESSAGE:-test}"
+result=$(docker run --network tp0_testing_net --rm alpine /bin/sh -c "echo '$TEST_MESSAGE' | nc server 12345")
 
 if [ "$result" == "$TEST_MESSAGE" ]; then
-    echo "action: test_echo_server | result: success"
+  echo "action: test_echo_server | result: success"
 else
-    echo "action: test_echo_server | result: fail"
+  echo "action: test_echo_server | result: fail"
 fi
