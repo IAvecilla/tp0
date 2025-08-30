06e73bd535adac4e9a2192f9c45230b11610e89c
IAvecilla <iavecilla@fi.uba.ar>
2025-08-30 14:14:38 -0300

    Solucionado ejercicio 3

--- validar-echo-server.sh
+++ validar-echo-server.sh
@@ -1,17 +1,5 @@
 #!/bin/bash
 
-# TEST_MESSAGE="${TEST_MESSAGE:-test}"
-# docker build -t server-verifier -f server-verifier-dockerfile .
-
-# echo "Testing server wih message: $TEST_MESSAGE"
-# result=$(docker run --network tp0_testing_net --rm server-verifier sh -c "echo '$TEST_MESSAGE' | nc server 12345")
-
-# if [ "$result" == "$TEST_MESSAGE" ]; then
-#     echo "action: test_echo_server | result: success"
-# else
-#     echo "action: test_echo_server | result: fail"
-# fi
-
 TEST_MESSAGE="${TEST_MESSAGE:-test}"
 result=$(docker run --network tp0_testing_net --rm alpine /bin/sh -c "echo '$TEST_MESSAGE' | nc server 12345")
 
