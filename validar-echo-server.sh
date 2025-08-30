c5cae2de3a4db214dab52d2146bca2feb9deef31
IAvecilla <iavecilla@fi.uba.ar>
2025-08-30 13:15:04 -0300

    Dockerfile y script para validar server

---
+++ validar-echo-server.sh
@@ -0,0 +1,11 @@
+TEST_MESSAGE="${TEST_MESSAGE:-test}"
+docker build -t server-verifier -f server-verifier-dockerfile .
+
+echo "Testing server wih message: $TEST_MESSAGE"
+result=$(docker run --network tp0_testing_net --rm server-verifier sh -c "echo '$TEST_MESSAGE' | nc server 12345")
+
+if [ "$result" == "$TEST_MESSAGE" ]; then
+    echo "action: test_echo_server | result: success"
+else
+    echo "action: test_echo_server | result: fail"
+fi
